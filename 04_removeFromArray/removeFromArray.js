a146d66135ba81619eb1e5866c2a317b029b2a12
EmmetMack <emmet.mack10@gmail.com>
2022-02-28 20:59:29 -0600

    completed remove from array exercise'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = Array.from(arguments);
+    let i = 0;
+    while (i < arr.length) {
+        for (let arg of args.slice(1,args.length)){
+            if (arr[i] === arg) {
+                arr.splice(i, 1);
+                i -= 1;
+            }
+        }
 
+        i++;
+    }
+    return arr;
 };
 
 // Do not edit below this line
