b4cc6660b3d9dd4868a494dacb1a1a4007f0b214
EmmetMack <emmet.mack10@gmail.com>
2022-02-28 21:17:34 -0600

    completed tempConvesion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return round((5/9) * (temp - 32), 1);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return round((32) +  ((9/5) * temp), 1);
 };
+
+//sauce: https://stackoverflow.com/questions/7342957/how-do-you-round-to-1-decimal-place-in-javascript
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
