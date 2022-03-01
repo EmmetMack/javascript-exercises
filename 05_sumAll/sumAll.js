6815e3ca84c9ccba291be9b33273e10463e73aea
EmmetMack <emmet.mack10@gmail.com>
2022-02-28 21:06:38 -0600

    completed sumAll problems

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    //loop through all the values then add them up
+    if (typeof(start) != 'number' || typeof(end) != 'number' || start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    let beginning = start < end ? start : end;
+    let finish = end > start ? end : start;
+    let sum = 0;
+    for (let i = beginning; i <= finish; i ++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
