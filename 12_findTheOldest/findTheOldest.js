b93f680b92fd742ec978e84fd78a8dad7ce89de2
EmmetMack <emmet.mack10@gmail.com>
2022-03-11 15:55:14 -0600

    Completed findTheOldest and passed all the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((geezer, person) => {
+        let geezerAge;
+        let newPersonAge;
+        if (geezer.yearOfDeath === undefined) {
+            geezerAge = new Date().getFullYear() - geezer.yearOfBirth;
+        } else {
+            geezerAge = geezer.yearOfDeath - geezer.yearOfBirth;
+        }
+        
+        if (person.yearOfDeath === undefined) {
+            newPersonAge = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            newPersonAge = person.yearOfDeath - person.yearOfBirth;
+        }
 
+        return geezerAge > newPersonAge ? geezer : person;
+    }, people[0]);
 };
 
 // Do not edit below this line
