f513ee34485ef969ac935c5eb84087c5479aed21
EmmetMack <emmet.mack10@gmail.com>
2022-03-11 15:38:32 -0600

    completed fibonacci exercise and passed all the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    //brainfarted the algo, got help from here: https://eskeype.github.io/2018/04/20/7-Fibonacci-Algorithms/
+    if (n < 0) { return "OOPS"};
+    let curr = 0;
+    let next = 1;
+    for (let i = 0; i < n; i++) {
+        let newNum = next + curr;
+        curr = next;
+        next = newNum;
+    }
 
+    return curr;
+    
 };
 
 // Do not edit below this line
